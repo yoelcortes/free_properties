b251e55340c1c6f6447dd92caf63332af82f787f
yoelcortes <yoelcortes@gmail.com>
2019-06-16 21:11:31 -0500

    update

---
+++ build/lib/free_properties/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 18 14:20:29 2019
+
+@author: Guest Group
+"""
+
+__all__ = ('FreeProperty' , 'PropertyFactory')
+
+from ._free_property import FreeProperty
+from ._property_factory import PropertyFactory
