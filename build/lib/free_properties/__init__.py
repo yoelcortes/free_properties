71177125ad4303d55087c33076173d36d5ba8cfb
yoelcortes <yoelcortes@gmail.com>
2019-12-06 05:56:03 -0600

    update

--- build/lib/free_properties/__init__.py
+++ build/lib/free_properties/__init__.py
@@ -5,7 +5,8 @@ Created on Fri Jan 18 14:20:29 2019
 @author: Guest Group
 """
 
-__all__ = ('FreeProperty' , 'PropertyFactory')
+__all__ = ('FreeProperty' , 'PropertyFactory', 'property_array')
 
 from ._free_property import FreeProperty
 from ._property_factory import PropertyFactory
+from ._property_array import property_array
